c084e008de7f7a31ac9321d41cf016af4f013664
peeweetje <petravos70@gmail.com>
2019-10-11 15:21:33 +0200

    Refactor: remove code no longer relevant.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { ButtonContainer } from "./Button";
 import Box from "./Box";
@@ -8,7 +7,6 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <ButtonContainer>Styled button</ButtonContainer>
         <Box
           color="black"
@@ -20,17 +18,6 @@ const App: React.FC = () => {
         >
           Tomato
         </Box>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
