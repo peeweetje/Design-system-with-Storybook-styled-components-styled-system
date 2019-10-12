025206423511d4969a168dcc918ada57251d0616
peeweetje <petravos70@gmail.com>
2019-10-12 22:13:16 +0200

    Feature: add a11y and knobs.

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,9 @@
-import { configure } from "@storybook/react";
+import {configure, addDecorator} from '@storybook/react';
+import {withA11y} from '@storybook/addon-a11y';
+import {withKnobs, text, boolean, number} from '@storybook/addon-knobs';
+
+addDecorator(withA11y);
+addDecorator(withKnobs, text, boolean, number);
 // automatically import all files ending in *.stories.tsx
 configure(
   require.context("../stories", true, /\.stories\.(tsx?|mdx)$/),
