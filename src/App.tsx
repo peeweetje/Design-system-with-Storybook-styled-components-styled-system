3ded5e1cff6db98156dfe83dd99787fb587c7ed4
peeweetje <petravos70@gmail.com>
2019-10-18 16:28:25 +0200

    Refactor: remove code no longer relevant.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import React from "react";
-import "./App.css";
 import { StyledButton } from "./Button";
 import Box from "./Box";
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      <header>
         <StyledButton onClick={() => console.log("clicked")}>
-          {" "}
           Styled button
         </StyledButton>
         <Box
