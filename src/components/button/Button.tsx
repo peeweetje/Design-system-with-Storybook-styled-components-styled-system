8ff0185b0ce1467fa06a5a79fb9f729f538b9633
PEEWEE\petra <petravos70@gmail.com>
2021-04-18 21:05:12 +0200

    Refactor: create a seperate styling file for the button.

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,5 +1,5 @@
 import React, { FC, ReactNode } from 'react';
-import styled, { css } from 'styled-components';
+import {StyledButton} from './button.styles'
 
 export interface ButtonProps {
   /** Description of onClick function which returns void **/
@@ -34,37 +34,4 @@ export const Button: FC<ButtonProps> = ({
 
 export default Button;
 
-export const StyledButton = styled.button<ButtonProps>`
-  border-radius: 3px;
-  border: 2px solid ${(props) => props.theme.colors.blues[3]};
-  margin: 0 0.5em;
-  padding: 0.5em 1em;
-  &:hover {
-    background-color: ${(props) => props.theme.colors.blues[5]};
-  }
-  ${(props) =>
-    props.primary &&
-    css`
-      background-color: ${(props) => props.theme.colors.blues[6]};
-      border: 2px solid ${(props) => props.theme.colors.blues[6]};
-      color: #fff;
-    `};
-  ${(props) =>
-    props.secondary &&
-    css`
-      background-color: ${(props) => props.theme.colors.greys[0]};
-      color: #000;
-    `};
-  ${(props) =>
-    props.disabled &&
-    css`
-      background-color: ${(props) => props.theme.colors.greys[4]};
-      color: black;
-      border: 2px solid ${(props) => props.theme.colors.greys[4]};
-      cursor: not-allowed;
-      &:hover {
-        background-color: ${(props) => props.theme.colors.greys[5]};
-        color: white;
-      }
-    `};
-`;
+
---
+++ src/components/button/button.styles.ts
@@ -0,0 +1,38 @@
+import styled, { css } from 'styled-components';
+import { ButtonProps } from './Button';
+
+
+export const StyledButton = styled.button<ButtonProps>`
+  border-radius: 3px;
+  border: 2px solid ${(props) => props.theme.colors.blues[3]};
+  margin: 0 8px;
+  padding: 8px 16px;
+  &:hover {
+    background-color: ${(props) => props.theme.colors.blues[5]};
+  }
+  ${(props) =>
+    props.primary &&
+    css`
+      background-color: ${(props) => props.theme.colors.blues[6]};
+      border: 2px solid ${(props) => props.theme.colors.blues[6]};
+      color: #fff;
+    `};
+  ${(props) =>
+    props.secondary &&
+    css`
+      background-color: ${(props) => props.theme.colors.greys[0]};
+      color: #000;
+    `};
+  ${(props) =>
+    props.disabled &&
+    css`
+      background-color: ${(props) => props.theme.colors.greys[4]};
+      color: black;
+      border: 2px solid ${(props) => props.theme.colors.greys[4]};
+      cursor: not-allowed;
+      &:hover {
+        background-color: ${(props) => props.theme.colors.greys[5]};
+        color: white;
+      }
+    `};
+`;
