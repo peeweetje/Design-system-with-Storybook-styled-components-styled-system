6b3cefd4563d0357bed56bb76d3d7b21a8e71893
PEEWEE\petra <petravos70@gmail.com>
2021-04-18 21:29:50 +0200

    Feature: create sidebar(w.i.p)

---
+++ src/documentation/components/sidebar/sidebar.styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const ContainerSidebar = styled.nav`
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    overflow: auto;
+    width: 11.625rem;
+    background: #333;
+    color: #fff;
+  `
+
---
+++ src/documentation/components/sidebar/sidebar.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react';
+import { ContainerSidebar } from './sidebar.styles';
+
+const Sidebar: FC = () => {
+  return <ContainerSidebar></ContainerSidebar>;
+};
+
+export default Sidebar;
