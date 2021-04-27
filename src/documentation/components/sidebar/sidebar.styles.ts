2bd33d932f37c0ac7880249bc081caf8981fb4e6
PEEWEE\petra <petravos70@gmail.com>
2021-04-27 18:51:40 +0200

    Refactor: Add links and styling.

--- src/documentation/components/sidebar/sidebar.styles.ts
+++ src/documentation/components/sidebar/sidebar.styles.ts
@@ -1,6 +1,9 @@
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 export const ContainerSidebar = styled.nav`
+    display: flex;
+    flex-direction: column;
     position: fixed;
     top: 0;
     bottom: 0;
@@ -8,6 +11,27 @@ export const ContainerSidebar = styled.nav`
     overflow: auto;
     width: 200px;
     background:  ${(props) => props.theme.colors.grey[8]};
-    color: #fff;
+    color:  ${(props) => props.theme.colors.grey[0]};
   `
+ export const SidebarTitle = styled.h1`
+    display: flex;
+    justify-content: center;
+    margin-top: 20px;
+  `
+
+ export const NavList = styled.ul`
+  list-style-type: none;
+  line-height: 2;
+  font-size: 1rem;
+`;
+
+ export const NavItem = styled.li`
+  display: flex;
+`;
+
+ export const StyledLink = styled(Link)`
+  color: ${(props) => props.theme.colors.grey[0]};
+  text-decoration: none;
+`;
+
 
--- src/documentation/components/sidebar/sidebar.tsx
+++ src/documentation/components/sidebar/sidebar.tsx
@@ -1,8 +1,25 @@
 import React, { FC } from 'react';
-import { ContainerSidebar } from './sidebar.styles';
+import {
+  ContainerSidebar,
+  SidebarTitle,
+  NavList,
+  NavItem,
+  StyledLink,
+} from './sidebar.styles';
 
 const Sidebar: FC = () => {
-  return <ContainerSidebar></ContainerSidebar>;
+  return (
+    <ContainerSidebar>
+      <SidebarTitle>
+        <StyledLink to='/'>Components</StyledLink>
+      </SidebarTitle>
+      <NavList>
+        <NavItem>
+          <StyledLink to='/button'>Button</StyledLink>
+        </NavItem>
+      </NavList>
+    </ContainerSidebar>
+  );
 };
 
 export default Sidebar;
