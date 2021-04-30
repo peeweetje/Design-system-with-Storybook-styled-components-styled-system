29414387afaa8edbe794d07c1db31334c6b425f7
PEEWEE\petra <petravos70@gmail.com>
2021-04-30 10:39:28 +0200

    Refactor: Add routing.Remove styling.

--- src/documentation/components/app/App.tsx
+++ src/documentation/components/app/App.tsx
@@ -1,16 +1,27 @@
 import React, { FC } from 'react';
-import { BtnContainer } from '../../../components/button/button.styles';
+import { Switch, Route } from 'react-router';
 import Sidebar from '../sidebar/sidebar';
-import { AppStyling } from './app.styles';
+import ButtonPage from '../../documentation-pages/button-page/button-page';
+import HomePage from '../../documentation-pages/home-page/home-page';
+import Header from '../../../components/header/header';
+import Layout from '../layout/layout';
 
 const App: FC = () => {
   return (
-    <AppStyling>
+    <>
       <Sidebar />
-      <BtnContainer primary='primary' onClick={() => console.log('clicked')}>
-        click on me
-      </BtnContainer>
-    </AppStyling>
+      <Layout>
+        <Switch>
+          <Header left='left' middle='middle' right='right' />
+          <Route exact path='/button'>
+            <ButtonPage />
+          </Route>
+          <Route exact path='/'>
+            <HomePage />
+          </Route>
+        </Switch>
+      </Layout>
+    </>
   );
 };
 
