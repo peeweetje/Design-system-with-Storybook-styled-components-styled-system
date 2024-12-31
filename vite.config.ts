6d6b0507f28fd9d28973ed660dd13a5445c20247
PEEWEE\petra <petravos70@gmail.com>
2024-12-31 20:56:42 +0100

    Remove code in vite config and update lock file.

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
   test: {
     globals: true,
     environment: 'jsdom',
