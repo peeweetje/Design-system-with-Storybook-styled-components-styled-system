5a505969abe60767ec3b5caba48bc61fff5cac71
PEEWEE\petra <petravos70@gmail.com>
2025-01-17 16:41:20 +0100

    Add config for shadcn.

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,8 @@
+// <reference types="vite/client"/>
+// <reference types= "vitest"/>
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +12,10 @@ export default defineConfig({
     environment: 'jsdom',
     css: true,
     setupFiles: './src/setup.ts',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
 });
