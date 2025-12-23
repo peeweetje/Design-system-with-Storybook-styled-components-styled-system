ea07f50616e94c5b3be1ad8e3cc6b66b54dc4649
peeweetje <petravos70@gmail.com>
2025-12-23 09:45:28 +0000

    Deploying to gh-pages from @ peeweetje/Design-system-with-Storybook-styled-components-styled-system@92071c7ebcc7c297e8807ac5e8485e4c812af2d7 🚀

---
+++ assets/formatter-QJ4M4OGQ-DoXrRn0l.js
@@ -0,0 +1,1 @@
+import{d as m,r as o}from"./blocks-BejT5ZC4.js";import{b as t}from"./iframe-CSmMzI2s.js";import"./preload-helper-PPVm8Dsz.js";import"./index-D6WreqlX.js";import"./jsx-runtime-u17CrQMm.js";var e=t(o(),1),_=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{_ as formatter};
